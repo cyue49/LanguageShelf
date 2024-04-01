a6cb218311935e4ad0887b8b64991c7cc84727f1
Chen Yue <yuechen049@gmail.com>
2024-03-31 22:37:14 -0400

    added default user profile pictures for all themes

--- LanguageShelf/LanguageShelf/Views/UserProfile/SignedInView.swift
+++ LanguageShelf/LanguageShelf/Views/UserProfile/SignedInView.swift
@@ -4,14 +4,47 @@ import Firebase
 struct SignedInView: View {
     @EnvironmentObject var userManager: UserAccountsManager
     
+    @State private var profilePic: UIImage?
+    
     var body: some View {
         if let user = userManager.currentUser {
             VStack (alignment: .center, spacing: 25) {
-                Circle()
-                    .fill(userManager.currentTheme.buttonColor)
-                    .stroke(userManager.currentTheme.secondaryColor, lineWidth: 2)
-                    .frame(width: 150, height: 150)
-                    .padding(.top)
+                switch userManager.currentTheme.name {
+                case "default":
+                    Image(uiImage: profilePic ?? UIImage(resource: .profileBlue))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .padding(.top)
+                        .clipShape(.circle)
+                case "light":
+                    Image(uiImage: profilePic ?? UIImage(resource: .profileLight))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .padding(.top)
+                        .clipShape(.circle)
+                case "dark":
+                    Image(uiImage: profilePic ?? UIImage(resource: .profileDark))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .padding(.top)
+                        .clipShape(.circle)
+                case "green":
+                    Image(uiImage: profilePic ?? UIImage(resource: .profileGreen))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .padding(.top)
+                        .clipShape(.circle)
+                default:
+                    Circle()
+                        .fill(userManager.currentTheme.buttonColor)
+                        .stroke(userManager.currentTheme.secondaryColor, lineWidth: 2)
+                        .frame(width: 150, height: 150)
+                        .padding(.top)
+                }
                 
                 EditTextFieldView(updateField: "username", inputText: user.username)
                 
